595646504e7d72513ecdd6a3604e59f8945b0ecb
mandooboo <jmlee3096@gmail.com>
2018-04-18 18:44:31 +0900

    Modified

---
+++ Step3/022.py
@@ -0,0 +1,9 @@
+#022
+
+name = 'Kebin'
+work = 'IT company'
+num1 = 5.3
+num2 = 4.7517
+print("I'm", name, "and work in", work)
+print("I'm {} and work in {}".format(name, work))
+print("This is {:8.2}, {:8.2}".format(num1,num2))
---
+++ Step3/023.py
@@ -0,0 +1,6 @@
+#023
+
+
+country = 'Korea'
+population = '50M'
+print("{} has about {} people".format(country, population))
